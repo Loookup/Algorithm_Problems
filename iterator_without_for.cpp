cb8865b4149077188e7d9d6143be4d5739efd2b7
Chulyong Lee <asphalt97@naver.com>
2023-07-10 16:53:02 +0900

    Add files via upload

---
+++ iterator_without_for.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+
+int main(){
+    std::vector<int> vec;
+
+    std::istream_iterator<int> cin_iter(std::cin);
+
+    std::istream_iterator<int> eos;
+
+    std::copy(cin_iter, eos, std::back_inserter(vec));
+
+    std::sort(begin(vec), end(vec));
+
+    std::ostream_iterator<int> cout_iter(std::cout, " ");
+
+    std::copy(vec.begin(), vec.end(), cout_iter);
+
+}
